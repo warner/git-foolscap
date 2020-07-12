2a7808ebd7eceeda87abaaec910b08b815205daf
Brian Warner <warner@lothar.com>
2020-07-12 14:00:42 -0700

    setup.py: update for py3, newer version of foolscap

--- setup.py
+++ setup.py
@@ -23,8 +23,8 @@ setup_args = {
         ],
     'platforms': ["any"],
 
-    'python_requires': "<3.0",
-    'install_requires': ["foolscap >= 0.8.0",
+    'python_requires': ">=3.5",
+    'install_requires': ["foolscap >= 20.4.0",
                          "magic-wormhole >= 0.10.5",
                          ],
 
